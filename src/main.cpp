88cd21ba1186b7e15f91ead57ac894dacf1e6880
Edouard5618 <edouard5618@gmail.com>
2025-11-20 09:29:39 -0500

    Set Fans to ON

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void loop()
         break;
     case eBioreactorState::TEST: // For the fluidic and heating system test
     {
-        setFansState(ON, OFF, OFF, OFF, OFF, OFF, ON);
+        setFansState(ON, ON, ON, ON, ON, ON, ON);
         // setPumpsSpeed(255, OFF, OFF, 255);
         setValvesState(CLOSE, CLOSE, CLOSE);
         setPressureChamberValvesState(OFF, OFF, OFF);
